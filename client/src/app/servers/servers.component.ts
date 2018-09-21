eb662a0bed13dccddc24923495ecb278a4a0b7ca
PadamSandhu <sandhp7@gmail.com>
2018-09-20 21:55:53 -0400

    Config

--- client/src/app/servers/servers.component.ts
+++ client/src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html', // Template URL is url to the html template, whereas template will let you write template code
+  templateUrl: './servers.component.html', // Template URL is url to the html template, whereas template will let you write template code .
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
